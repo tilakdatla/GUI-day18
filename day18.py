91d0eace04946083f54ade1aaaaf4d3bee383832
Tilak Datla <146624524+tilakdatla@users.noreply.github.com>
2024-04-23 18:36:18 +0530

    Add files via upload

---
+++ day18.py
@@ -0,0 +1,122 @@
+import turtle
+from turtle import Turtle,Screen
+tim=Turtle()
+import random
+
+
+#challenge 1
+
+# color=["red","blue","green","yellow","pink","orange","black","violet"]
+#
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#      tim.forward(100)
+#      tim.right(angle)
+#
+# for i in range(3,11):
+#     tim.color(random.choice(color))
+#     draw_shape(i)
+
+#challege 2
+
+# turtle.colormode(255)
+# direction=[0,90,180,270]
+# tim.pensize(10)
+# tim.speed(20)
+#
+# def random_color():
+#     r=random.randint(0,255)
+#     g=random.randint(0,255)
+#     b=random.randint(0,255)
+#     random_color=(r,g,b)
+#     return random_color
+#
+# for i in range(1,200):
+#     tim.color(random_color())
+#     tim.forward(20)
+#     tim.setheading(random.choice(direction))
+#
+
+
+# challenge 3
+#
+# turtle.colormode(255)
+# tim.speed(30)
+# def random_color():
+#     r=random.randint(0,255)
+#     g=random.randint(0,255)
+#     b=random.randint(0,255)
+#     random_color=(r,g,b)
+#     return random_color
+#
+# for i in range(0,80):
+#     tim.color(random_color())
+#     tim.circle(100)
+#     tim.forward(0)
+#     tim.right(5)
+
+
+
+#challenge 4
+#
+# turtle.colormode(255)
+# color_list=[ (52, 107, 132), (179, 77, 31), (202, 142, 31), (115, 155, 171), (124, 79, 99), (122, 175, 156), (229, 236, 239), (226, 198, 131), (242, 247, 244), (192, 87, 108), (11, 50, 64), (55, 38, 19), (45, 168, 126), (47, 127, 123), (200, 121, 143), (168, 21, 29), (228, 92, 77), (244, 162, 160), (38, 32, 35), (2, 25, 24), (78, 147, 171), (170, 23, 18), (19, 79, 90), (101, 126, 158), (235, 166, 171), (177, 204, 185), (49, 62, 84)]
+# tim.penup()
+# tim.hideturtle()
+# tim.setheading(225)
+# tim.forward(300)
+# tim.setheading(0)
+# number_of_dots=100
+# tim.speed(20)
+#
+# for i in range(1,number_of_dots+1):
+#     tim.dot(20,random.choice(color_list))
+#     tim.forward(50)
+#     if i %10==0:
+#         tim.setheading(90)
+#         tim.forward(50)
+#         tim.setheading(180)
+#         tim.forward(500)
+#         tim.setheading(0)
+#
+#
+#
+# screen=Screen()
+# screen.exitonclick()
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
